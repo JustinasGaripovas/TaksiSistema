d2e74e602c7779c2d67d8df8ece08f40ebc4dee7
Justinas Garipovas <justinas.garipovas@telesoftas.com>
2020-05-23 15:23:29 +0300

    feat: Added communication serive

---
+++ src/Service/CommunicationService.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Service;
+
+
+use App\Entity\Order;
+
+class CommunicationService
+{
+    public function CallToNumber(Order $order)
+    {
+        
+    }
+
+    public function WriteLetterToEmail(string $email)
+    {
+
+    }
+}
