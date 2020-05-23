53bc67c2374eb2856b97ecc8fab8c618b4594aee
Justinas Garipovas <justinas.garipovas@telesoftas.com>
2020-05-23 15:28:33 +0300

    feat: Created User trait

--- src/Service/CommunicationService.php
+++ src/Service/CommunicationService.php
@@ -5,12 +5,20 @@ namespace App\Service;
 
 
 use App\Entity\Order;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class CommunicationService
 {
     public function CallToNumber(Order $order)
     {
-        
+        //TODO: Some validation, or some must do actions
+
+        return new JsonResponse(
+            [
+//                'driver_name' => $order->getDriver()->getUser()
+            ]
+        );
+
     }
 
     public function WriteLetterToEmail(string $email)
