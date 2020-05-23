bd222ed80fba472ddec5332e60f69ec5c5195369
Justinas Garipovas <justinas.garipovas@telesoftas.com>
2020-05-23 15:05:22 +0300

    feat: Migration to fix database

---
+++ src/Migrations/Version20200523120505.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200523120505 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE car_type (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, price_per_km INT NOT NULL, price_per_minute INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE `order` ADD status INT DEFAULT NULL, CHANGE driver_id driver_id INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE car_type');
+        $this->addSql('ALTER TABLE `order` DROP status, CHANGE driver_id driver_id INT NOT NULL');
+    }
+}
