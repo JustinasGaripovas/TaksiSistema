42e5ea6db6a61c6c3a0a48986e6b2f636ca057ed
Tomas Žarskis <t.zarskis@gmail.com>
2020-05-25 00:10:57 +0300

    Created new entity for driver request

---
+++ src/Migrations/Version20200524211024.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200524211024 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F5299398A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_F5299398A76ED395 ON `order` (user_id)');
+        $this->addSql('ALTER TABLE driver_request DROP INDEX UNIQ_FE68EBFFA76ED395, ADD INDEX IDX_FE68EBFFA76ED395 (user_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE driver_request DROP INDEX IDX_FE68EBFFA76ED395, ADD UNIQUE INDEX UNIQ_FE68EBFFA76ED395 (user_id)');
+        $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F5299398A76ED395');
+        $this->addSql('DROP INDEX IDX_F5299398A76ED395 ON `order`');
+    }
+}
